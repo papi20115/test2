61b1aab133fe2ee5022de8a11b07ba0234124709
papi20115 <meshachadarighofua@gmail.com>
2026-09-01 00:47:18 -0700

    Add files via upload

---
+++ web.js
@@ -0,0 +1,15 @@
+let answer;
+
+document.getElementById("mybu"). onclick= function(){
+    answer=document.getElementById("myp").textContent=`ok`
+    document.getElementById("myh1").textContent=`r u sure`
+    document.getElementById("myp2").textContent=`r u a boy or girl `
+        
+}
+
+document.getElementById("mybu2").onclick=function(){
+    answer=document.getElementById("myp").textContent=`good`
+    document.getElementById("myh1").textContent=`Thanks for  not telling a lie`
+    document.getElementById("myp2").textContent=`click next`
+}
+
